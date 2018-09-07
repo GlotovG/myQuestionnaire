7deb722c211e26c140f2140f0f05d251190f27f4
Glotov Gennady <glotovg@gmail.com>
2018-09-07 23:51:21 +0300

    Initial Commit

---
+++ myQuestionnaire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myQuestionnaire
+//
+//  Created by Gennadiy Glotov on 07.09.2018.
+//  Copyright © 2018 Gennadiy Glotov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
